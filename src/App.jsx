58b7b71c96697083652f49409314bea8d7612f34
tooreagen <tooreagen@gmail.com>
2025-11-15 15:57:50 +0200

    change site to off mode

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,25 @@
-import { GlobalStyle } from './components/GlobalStyle';
-import { Routes, Route } from 'react-router-dom';
-import Layout from './components/Layout/Layout';
-import MainPage from './components/MainPage/MainPage';
+import { GlobalStyle } from "./components/GlobalStyle";
+import { Routes, Route } from "react-router-dom";
+import Layout from "./components/Layout/Layout";
+import MainPage from "./components/MainPage/MainPage";
 
 export const App = () => {
   return (
-    <>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<MainPage />} />
-          <Route path="*" element={<MainPage />} />
-        </Route>
-      </Routes>
-      <GlobalStyle />
-    </>
+    //Заглушка, що сайт не працює
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Сайт тимчасово не працює</h1>
+      <p>Просимо вибачення за незручності.</p>
+      <p>Зверніться до адміністратора</p>
+    </div>
+    //Заглушка, що сайт не працює
+    // <>
+    //   <Routes>
+    //     <Route path="/" element={<Layout />}>
+    //       <Route index element={<MainPage />} />
+    //       <Route path="*" element={<MainPage />} />
+    //     </Route>
+    //   </Routes>
+    //   <GlobalStyle />
+    // </>
   );
 };
